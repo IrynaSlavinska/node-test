1c1be0bd3f817e16d349589e35aabe59ee6a07ca
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-01-19 12:19:35 +0200

    ended

--- 03_express_basics/index.js
+++ 03_express_basics/index.js
@@ -7,6 +7,46 @@ const app = express();
 
 // MIDDLEWARES ==========================================
 app.use(express.json());
+app.use(cors());
+
+// global custom middleware
+// якщо не прописаний ендпоінт, то мідлвар буде спрацьовувати на УСІ запити
+app.use((req, res, next) => {
+  console.log("Hello from middleware!!!");
+
+  req.time = new Date().toLocaleString("uk-UA");
+
+  // зробили маніпуляції з ріквестом, респонс вона не віддає і викликає некст
+  // (наступний мідлвар, якщо він є або перекине в наступний контроллер)
+  next();
+});
+
+app.use("/users/:userID", async (req, res, next) => {
+  try {
+    const { userID } = req.params;
+
+    if (userID.length < 10) {
+      res.status(400).json({ message: "Failed (((" });
+      return;
+    }
+
+    const usersDB = await fs.readFile("users.json");
+    const users = JSON.parse(usersDB);
+
+    const user = users.find((el) => el.id === req.params.userID);
+
+    if (!user) {
+      res.status(404).json({
+        message: "User is not found",
+      });
+    }
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // CONTROLLERS ==========================================
 app.get("/ping", (req, res) => {
@@ -14,7 +54,8 @@ app.get("/ping", (req, res) => {
   //   res.send("<h1>Hello from server!!!</h1>");
   //   res.sendStatus(201);
   res.status(200).json({
-    message: "Hello from JSON",
+    // message: "Hello from JSON",
+    message: "PONG !",
   });
 });
 
@@ -79,14 +120,16 @@ app.get("/users", async (req, res) => {
 app.get("/users/:userID", async (req, res) => {
   //   console.log(req.params);
   try {
-    const usersDB = await fs.readFile("users.json");
-    const users = JSON.parse(usersDB);
+    // const usersDB = await fs.readFile("users.json");
+    // const users = JSON.parse(usersDB);
 
-    const user = users.find((el) => el.id === req.params.userID);
+    // const user = users.find((el) => el.id === req.params.userID);
 
     res.status(201).json({
       message: "Success!",
-      user,
+      //   user,
+      user: req.user,
+      time: req.time,
     });
   } catch (error) {
     console.log(error);
